c3ddfbf662289497e24b5019878ea91afc11274f
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-05 01:36:36 +0100

    ultima versione

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-const listspesa=["carote", "formaggio", "piselli", "patate", "ak-47", "3 pacchi di 7.62x39"];
+let listspesa=["carote", "formaggio", "piselli", "patate", "ak-47", "3 pacchi di 7.62x39"];
 console.log(listspesa);
 
 
-i = 0
+i=0
 while (i< listspesa.length) {
-    document.getElementById("spesa").innerHTML= listspesa;
+    document.getElementById("lista").innerHTML= listspesa;
     i++;
-    document.getElementById("spesa").innerHTML= listspesa;
+    document.getElementById("lista").innerHTML= listspesa;
 }
-
-
 
